5553c758f31306b27dd428c5bd9809bd287f2c2f
Muhammed <muhammadburhan964@gmail.com>
2022-04-20 21:37:20 +0300

    Blog Pagination

--- controllers/BlogController.js
+++ controllers/BlogController.js
@@ -2,7 +2,12 @@ const { StatusCodes } = require("http-status-codes");
 const Blog = require("../model/blog");
 
 const getBlogs = async (req, res) => {
-  const blog = await Blog.find({}).sort("createdAt");
+  let result = Blog.find({});
+  const limit = Number(req.query.limit) || 10;
+  const page = Number(req.query.page) || 1;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+  const blog = await result;
   res.status(StatusCodes.OK).json({ blog });
 };
 
